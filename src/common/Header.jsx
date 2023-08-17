06fd11a2e4088151f62f9c5161c79549f0a45439
yui62yui <you@example.com>
2023-08-17 22:48:58 +0900

    Feat / Header,Footer 삽입

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,7 +1,40 @@
+import { useAtom } from "jotai";
 import React from "react";
+import { userAtom } from "../store/userAtom";
+import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 const Header = () => {
-  return <div></div>;
+  const [user, setUser] = useAtom(userAtom);
+  const auth = getAuth();
+
+  const logoutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        alert("로그아웃 되었습니다");
+      })
+      .catch(() => {
+        alert("로그아웃에 실패하였습니다. 다시 시도해 주세요");
+      });
+  };
+
+  return (
+    <div>
+      {!!user ? (
+        <div>
+          <Link to={"/"}>홈</Link>
+          <Link to={`/mypage/${user.uid}`}>내프로필</Link>
+          <span onClick={logoutHandler}>로그아웃</span>
+        </div>
+      ) : (
+        <div>
+          <Link to={"/"}>홈</Link>
+          <Link to={"/login"}>로그인</Link>
+          <Link to={"/signup"}>회원가입</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Header;
--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 function Layout() {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
