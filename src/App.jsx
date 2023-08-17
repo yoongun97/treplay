b43e5bf4bd409e55cf8c4f37e3cd2248b9ce7008
Cho Yoongeon <tesung5470@naver.com>
2023-08-17 14:25:39 +0900

    프로젝트, 라우터 세팅

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Layout from "./common/Layout";
+import MainPage from "./pages/mainpage/MainPage";
+import NationPage from "./pages/nationpage/NationPage";
+import CategoryPage from "./pages/categorypage/CategoryPage";
+import DetailPage from "./pages/detailpage/DetailPage";
+import Create from "./pages/create/Create";
+import Edit from "./pages/edit/Edit";
+import MyPage from "./pages/mypage/MyPage";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/list" element={<NationPage />} />
+        <Route path="/list/:category" element={<CategoryPage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/mypage/:uid" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <>
+            <div>없는 페이지입니다.</div>
+            <Link to="/">홈으로 이동</Link>
+          </>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
