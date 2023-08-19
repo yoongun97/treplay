40abb210cbee90f178b417e7f7756d45f999050f
Cho Yoongeon <tesung5470@naver.com>
2023-08-19 11:24:21 +0900

    코드 정리, 주석 작성

--- src/components/PlaceMap.jsx
+++ src/components/PlaceMap.jsx
@@ -7,11 +7,15 @@ function PlaceMap({ postAddress }) {
   useEffect(() => {
     // Google Maps API 스크립트를 동적으로 로드
     const script = document.createElement("script");
+    //스크립트 엘리먼트를 생성하고, API 키 및 필요한 라이브러리 정보를 포함한 URL을 설정
     script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`;
     script.async = true;
     script.defer = true;
+    // 스크립트를 비동기적으로 로드하도록 설정
     script.onload = initMap;
+    //스크립트 로드가 완료되면 initMap 함수를 호출
     document.head.appendChild(script);
+    //스크립트 엘리먼트를 <head> 요소에 추가
 
     // 컴포넌트가 언마운트될 때 스크립트 제거
     return () => {
@@ -22,17 +26,21 @@ function PlaceMap({ postAddress }) {
   // 지도 초기화 함수
   const initMap = () => {
     const geocoder = new window.google.maps.Geocoder();
+    // 주소를 좌표로 변환합니다.
     geocoder.geocode({ address: postAddress }, (results, status) => {
       if (status === "OK" && results[0]) {
         const location = results[0].geometry.location;
+        // 변환된 좌표를 기반으로 지도를 초기화하고 Map 객체를 생성합니다.
         const map = new window.google.maps.Map(mapElement.current, {
           zoom: 17,
           center: location,
         });
+        // 변환된 좌표에 마커를 추가하여 해당 위치를 표시
         const marker = new window.google.maps.Marker({
           position: location,
           map,
         });
+        // 변환된 주소를 setAddress를 통해 상태에 업데이트
         setAddress(results[0].formatted_address);
       } else {
         alert("주소를 가져올 수 없습니다.");
@@ -54,77 +62,3 @@ function PlaceMap({ postAddress }) {
 }
 
 export default PlaceMap;
-
-//   const mapElement = useRef(null);
-//   const [address, setAddress] = useState(""); // 주소 상태 추가
-//   const [initialLocation, setInitialLocation] = useState({
-//     lat: 37.6093347203718,
-//     lng: 126.934340439756,
-//   });
-
-//   const geocoder = new window.google.maps.Geocoder();
-//   geocoder.geocode({ address: postAddress }, (results, status) => {
-//     if (status === "OK" && results[0]) {
-//       const location = results[0].geometry.viewport;
-//       console.log(location);
-//       setInitialLocation({ lat: location.Va.lo, lng: location.Ia.lo });
-//     } else {
-//       alert("주소를 가져올 수 없습니다.");
-//     }
-//   });
-
-//   // 지도 api스크립트를 동적으로 로딩하는 역할
-//   const loadScript = useCallback((url) => {
-//     const firstScript = window.document.getElementsByTagName("script")[0];
-//     const newScript = window.document.createElement("script");
-//     newScript.src = url;
-//     newScript.async = true;
-//     newScript.defer = true;
-//     firstScript?.parentNode?.insertBefore(newScript, firstScript);
-//   }, []);
-
-//   // script에서 google map api를 가져온 후에 실행될 callback 함수
-//   // 지도 초기화 함수
-//   const initMap = useCallback(() => {
-//     // google이 정의되었을 때만 지도와 마커 생성
-//     if (!mapElement.current || !google) return;
-
-//     const map = new google.maps.Map(mapElement.current, {
-//       zoom: 17,
-//       center: initialLocation,
-//     });
-//     // 마커 생성
-//     const marker = new google.maps.Marker({
-//       position: initialLocation,
-//       map,
-//     });
-
-//     // 마커 클릭 시 주소 가져오기
-//     const geocoder = new google.maps.Geocoder();
-//     marker.addListener("click", () => {
-//       geocoder.geocode({ location: initialLocation }, (results, status) => {
-//         if (status === "OK" && results[0]) {
-//           setAddress(results[0].formatted_address);
-//         } else {
-//           setAddress("주소를 가져올 수 없습니다.");
-//         }
-//         console.log(results);
-//         console.log(status);
-//       });
-//     });
-//   }, []);
-
-//   useEffect(() => {
-//     const script = window.document.getElementsByTagName("script")[0];
-//     const includeCheck = script.src.startsWith(
-//       "https://maps.googleapis.com/maps/api"
-//     );
-
-//     // script 중복 호출 방지
-//     if (includeCheck) return initMap();
-
-//     window.initMap = initMap;
-//     loadScript(
-//       "https://maps.googleapis.com/maps/api/js?key=AIzaSyArdwSvnMVGKTD_LUxm_pGKnDDnrUSeZDY&callback=initMap&language=en"
-//     );
-//   }, [initMap, loadScript]);
