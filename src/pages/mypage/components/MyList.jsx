c746d3b9fc2f8b46938752c307db9e5d3b60a58a
yui62yui <you@example.com>
2023-08-22 18:51:28 +0900

    Feat / 저장한 글, 작성한 글 리스트 컴포넌트 분리 #19

---
+++ src/pages/mypage/components/MyList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function MyList({ myPosts, allLikedData }) {
+  return (
+    <div>
+      {myPosts?.map((post) => {
+        const likedCount = allLikedData?.filter(
+          (data) => data.postId === post.id && data.state === "like"
+        );
+        const dislikedCount = allLikedData?.filter(
+          (data) => data.postId === post.id && data.state === "dislike"
+        );
+        return (
+          <div className="myListBox" key={post.id}>
+            <div className="ImgBox">이미지</div>
+            <div className="ContentInfo">
+              <span>{post.author}</span>
+              <span>또가요({likedCount.length})</span>
+              <span>안가요({dislikedCount.length})</span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MyList;
