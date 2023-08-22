77e3e93a7b328d566648144ef85d5299121dd19e
yui62yui <you@example.com>
2023-08-22 19:35:38 +0900

    Feat / 게시물 이미지 불러오기 #19

--- src/pages/mypage/components/SavedList.jsx
+++ src/pages/mypage/components/SavedList.jsx
@@ -10,9 +10,15 @@ function SavedList({ savedPosts, allLikedData }) {
         const dislikedCount = allLikedData?.filter(
           (data) => data.postId === post.id && data.state === "dislike"
         );
+        const postImg = post.postImgs;
+        console.log(postImg);
         return (
-          <div className="myListBox" key={post.id}>
-            <div className="ImgBox">이미지</div>
+          <div className="ListBox" key={post.id}>
+            {postImg ? (
+              <img src={post.postImgs} alt="이미지" width="100px" />
+            ) : (
+              <img src="" alt="이미지 없음" width="100px"></img>
+            )}
             <div className="ContentInfo">
               <span>{post.author}</span>
               <span>또가요({likedCount.length})</span>
