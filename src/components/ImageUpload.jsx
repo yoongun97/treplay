608fa4a0bb90f5c9eef826ef59a9a9770ff9f323
Cho Yoongeon <tesung5470@naver.com>
2023-08-23 17:31:34 +0900

    Fix / 이미지 업로드 오류 해결

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -9,7 +9,7 @@ import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
 
-function ImageUpload({ content }) {
+function ImageUpload() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [post, setPost] = useAtom(postAtom);
   const [user] = useAtom(userAtom);
@@ -33,41 +33,50 @@ function ImageUpload({ content }) {
   const handleUpload = async (e) => {
     e.preventDefault();
 
-    if (selectedFiles.length === 0) {
+    if (post.nation === "") {
+      alert("나라를 선택해주세요.");
+    } else if (post.category === "") {
+      alert("카테고리를 선택해주세요.");
+    } else if (post.category === "") {
+      alert("카테고리를 선택해주세요.");
+    } else if (post.placeName === "") {
+      alert("장소를 선택해주세요.");
+    } else if (post.postContent === "") {
+      alert("내용을 입력해주세요.");
+    } else if (selectedFiles.length === 0) {
       alert("파일을 선택해주세요.");
-      return;
+    } else {
+      const newDownloadURLs = []; // 새로운 downloadURL 배열 생성
+
+      // 각 이미지 파일을 순회하며 업로드
+      for (const file of selectedFiles) {
+        // ref 함수를 이용해서 Storage 내부 저장할 위치를 지정하고, uploadBytes 함수를 이용해서 파일을 저장합니다.
+        const imageRef = ref(storage, `${auth.currentUser.uid}/${file.name}`);
+        await uploadBytes(imageRef, file);
+
+        // 파일 URL 가져오기
+        const downloadURL = await getDownloadURL(imageRef);
+        newDownloadURLs.push(downloadURL); // 새로운 downloadURL 배열에 추가
+      }
+
+      // 업로드된 이미지 URL을 포스트에 추가
+      const updatedPost = { ...post, postImgs: newDownloadURLs };
+
+      // 포스트 업데이트 후 데이터베이스에 추가
+      addMutation.mutate(updatedPost);
+
+      // 업로드 후 선택한 파일 목록 초기화
+      setSelectedFiles([]);
+      document.getElementById("file-input").value = ""; // 파일 선택 초기화
     }
-
-    const newDownloadURLs = []; // 새로운 downloadURL 배열 생성
-
-    // 각 이미지 파일을 순회하며 업로드
-    for (const file of selectedFiles) {
-      // ref 함수를 이용해서 Storage 내부 저장할 위치를 지정하고, uploadBytes 함수를 이용해서 파일을 저장합니다.
-      const imageRef = ref(storage, `${auth.currentUser.uid}/${file.name}`);
-      await uploadBytes(imageRef, file);
-
-      // 파일 URL 가져오기
-      const downloadURL = await getDownloadURL(imageRef);
-      newDownloadURLs.push(downloadURL); // 새로운 downloadURL 배열에 추가
-      setPost({ ...post, postImgs: newDownloadURLs });
-    }
-
-    addMutation.mutate(e);
-
-    // 업로드 후 선택한 파일 목록 초기화
-    setSelectedFiles([]);
-    document.getElementById("file-input").value = ""; // 파일 선택 초기화
   };
 
-  const addMutation = useMutation(async (event) => {
-    event.preventDefault();
-
+  const addMutation = useMutation(async (post) => {
     const newPost = {
       ...post,
       date: date,
       author: user.displayName,
       uid: user.uid,
-      postContent: content,
     };
 
     // Firestore에서 'todos' 컬렉션에 대한 참조 생성하기
--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import SelectBox from "../../components/selectBox/SelectBox";
-import PlaceAddress from "../../components/place/PlaceAddress";
 import ImageUpload from "../../components/ImageUpload";
-import Autocomplete from "react-google-autocomplete";
 import { useAtom } from "jotai";
 import { postAtom } from "../../store/postAtom";
 import PlaceSearch from "../../components/place/PlaceSearch";
 
 function Create() {
-  const [content, setContent] = useState();
   const [post, setPost] = useAtom(postAtom);
 
   return (
@@ -21,14 +18,14 @@ function Create() {
 
       <div style={{ margin: "20px", backgroundColor: "gray" }}>
         <textarea
-          value={content}
+          value={post.postContent}
           placeholder="내용을 작성하는 공간입니다."
           onChange={(e) => {
-            setContent(e.target.value);
+            setPost({ ...post, postContent: e.target.value });
           }}
         ></textarea>
       </div>
-      <ImageUpload content={content} style={{ margin: "20px" }} />
+      <ImageUpload style={{ margin: "20px" }} />
     </>
   );
 }
