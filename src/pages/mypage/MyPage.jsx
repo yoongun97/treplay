950a3ab4664882259195f960df5132440282d3de
yui62yui <you@example.com>
2023-08-24 13:06:14 +0900

    Refactor / 마이페이지에서 닉네임 컴포넌트 분리 #19

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -2,43 +2,29 @@ import { useAtom } from "jotai";
 import React, { useEffect, useState } from "react";
 import { userAtom } from "../../store/userAtom";
 import Unloggined from "../../common/Unloggined";
-import {
-  collection,
-  doc,
-  getDocs,
-  query,
-  updateDoc,
-  where,
-} from "firebase/firestore";
-import { auth, db } from "../../firebaseConfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
 import { useParams } from "react-router-dom";
 import SavedList from "./components/SavedList";
 import MyList from "./components/MyList";
-import { updateProfile } from "firebase/auth";
 import { useQuery } from "react-query";
 import ProfileImage from "./components/ProfileImage";
+import Nickname from "./components/Nickname";
 
 function MyPage() {
   const [user] = useAtom(userAtom);
   const userUidObject = useParams();
   const userUid = userUidObject.uid;
 
-  const [isMyListActived, setIsMyListActived] = useState(true);
-  const [isEditorAcitved, setIsEditorActived] = useState(false);
-
   const [allData, setAllData] = useState([]);
-
   const [ownData, setOwnData] = useState([]);
   const [allLikedData, setAllLikedData] = useState([]);
   const [myPosts, setMyPosts] = useState([]);
   const [savedPosts, setSavedPosts] = useState([]);
-
-  // 입력 받는 새로운 닉네임
-  const [newNickname, setNewNickname] = useState(user?.displayName);
+  const [isMyListActived, setIsMyListActived] = useState(true);
 
   const fetchData = async () => {
     // 유저 데이터
-
     const userQ = query(collection(db, "users"));
     const querySnapshot = await getDocs(userQ);
     const data = querySnapshot.docs.map((doc) => ({
@@ -92,43 +78,6 @@ function MyPage() {
     setIsMyListActived(true);
   };
 
-  // 닉네임 수정 버튼 클릭 핸들러
-  const startEditNameHandler = () => {
-    setIsEditorActived(true);
-  };
-
-  // 닉네임 중복 검사 및 수정 완료 핸들러
-  const endEditNameHandler = async () => {
-    try {
-      const usedNickname = allData.filter(
-        (item) => item.nickname === newNickname
-      );
-      if (!!newNickname === false) {
-        return alert("닉네임을 입력해 주세요");
-      } else if (usedNickname.length > 0) {
-        return alert(
-          "이미 사용 중인 닉네임입니다. 다른 닉네임을 사용해 주세요."
-        );
-      } else if (usedNickname.length === 0) {
-        setIsEditorActived(false);
-
-        // 1. firebase auth 정보 업데이트
-        updateProfile(auth.currentUser, {
-          displayName: newNickname,
-        });
-        // 2. firestore users db 정보 업데이트
-        const userRef = doc(db, "users", `${ownData.id}`);
-        await updateDoc(userRef, { nickname: newNickname });
-
-        fetchData();
-
-        return alert("닉네임 수정 완료!");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   // 리액트 쿼리로 로딩/에러 처리
 
   const { isLoading, iserror, error } = useQuery("userData", fetchData);
@@ -148,49 +97,13 @@ function MyPage() {
           <div className="UserInfoInner">
             <p>마이페이지</p>
             <div>
-              <ProfileImage />
-              <div>
-                {/* SNS 이용자는 닉네임 못 바꾸게 함 */}
-                {ownData === undefined ? (
-                  <div>
-                    <input type="text" value={user.displayName} disabled />
-                    <p>SNS 로그인 사용 시 닉네임을 수정할 수 없습니다.</p>
-                  </div>
-                ) : (
-                  <div>
-                    {/* 닉네임 인풋 */}
-                    {isEditorAcitved ? (
-                      <input
-                        type="text"
-                        value={newNickname}
-                        onChange={(e) => {
-                          setNewNickname(e.target.value);
-                        }}
-                      />
-                    ) : (
-                      <input type="text" value={user.displayName} disabled />
-                    )}
-                    {/* 닉네임 수정 버튼 */}
-                    {isEditorAcitved ? (
-                      <button
-                        onClick={() => {
-                          endEditNameHandler();
-                        }}
-                      >
-                        완료
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => {
-                          startEditNameHandler();
-                        }}
-                      >
-                        수정
-                      </button>
-                    )}
-                  </div>
-                )}
-              </div>
+              {/* 프로필 이미지/닉네임 컴포넌트 분리 */}
+              <ProfileImage fetchData={fetchData} />
+              <Nickname
+                ownData={ownData}
+                allData={allData}
+                fetchData={fetchData}
+              />
             </div>
           </div>
           <div className="ListContainer">
