bc223ebb999d4742b18b981f1c0c76c5629e39c9
yui62yui <you@example.com>
2023-08-24 22:39:08 +0900

    refactor / 베스트플레이스 컴포넌트 분리 #27

--- src/pages/nationpage/NationPage.jsx
+++ src/pages/nationpage/NationPage.jsx
@@ -3,6 +3,7 @@ import Category from "./components/Category";
 import MiddleBanner from "./components/MiddleBanner";
 import Preview from "./components/Preview";
 import EventBanner from "./components/EventBanner";
+import BestPlace from "./components/BestPlace";
 
 function NationPage() {
   // category 파라미터를 가져옴
@@ -15,25 +16,7 @@ function NationPage() {
       <MiddleBanner />
       <Preview />
       <EventBanner />
-      <div className="BestPlaceContainer">
-        <h2>베스트 또갈집</h2>
-        <div className="BestPlaceBox">
-          <div className="BestPlaceImgBox">이미지</div>
-          <div className="BestPlacePhrasesBox">
-            <h3>[맛집] 부평 우와</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
-              libero totam cupiditate suscipit, nesciunt consequatur deleniti
-              incidunt alias dolorum ullam recusandae reiciendis sed temporibus
-              odio quidem consequuntur? Laboriosam, mollitia quas!
-            </p>
-            <button className="MoreInfoButton">
-              <span>더 알아보기</span>
-              <img src="" alt=">" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <BestPlace />
     </div>
   );
 }
