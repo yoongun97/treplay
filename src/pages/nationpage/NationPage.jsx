ced67da0258b626b3acd530bc1becc584a778250
yui62yui <you@example.com>
2023-08-24 21:59:07 +0900

    refactor / 이벤트배너 분리 #27

--- src/pages/nationpage/NationPage.jsx
+++ src/pages/nationpage/NationPage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
 import Category from "./components/Category";
 import MiddleBanner from "./components/MiddleBanner";
 import Preview from "./components/Preview";
+import EventBanner from "./components/EventBanner";
 
 function NationPage() {
   // category 파라미터를 가져옴
-  const { nation } = useParams();
 
   return (
     <div className="Container">
@@ -15,25 +14,7 @@ function NationPage() {
       <Category />
       <MiddleBanner />
       <Preview />
-      <div className="EventBannerContainer">
-        <div>이미지</div>
-        <div className="EventBannerLeftBox">
-          <h3>#먹방대전! 또갈 맛집 투표하기</h3>
-          <p>
-            {nation}에서 기억에 남는 맛집이 있나요? <br /> 최고의 맛집에
-            또가요를 남겨 주세요
-          </p>
-          <Link to={`/${nation}/맛집`}>보러가기</Link>
-        </div>
-        <div className="EventBannerRightBox">
-          <h3>#나만의 또갈집 자랑하기</h3>
-          <p>
-            {nation}이라면 역시 이곳! <br /> 나의 원픽 추천 장소를 자랑해
-            주세요!
-          </p>
-          <Link to={`/create`}>자랑하기</Link>
-        </div>
-      </div>
+      <EventBanner />
       <div className="BestPlaceContainer">
         <h2>베스트 또갈집</h2>
         <div className="BestPlaceBox">
