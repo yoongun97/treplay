91e1d23b2b721b416bff93deb350edc3c0943250
yui62yui <you@example.com>
2023-08-25 22:17:25 +0900

    Feat / Preview 화면에서 버튼에 따라 다른 리스트 불러오기 #27

--- src/pages/nationpage/NationPage.jsx
+++ src/pages/nationpage/NationPage.jsx
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from "./components/Category";
 import MiddleBanner from "./components/MiddleBanner";
 import Preview from "./components/Preview";
 import EventBanner from "./components/EventBanner";
 import BestPlace from "./components/BestPlace";
 import MainCarousel from "../../components/imageslide/MainCarousel";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+import { useQuery } from "react-query";
 
 function NationPage() {
   // category 파라미터를 가져옴
+
+  const [posts, setPosts] = useState([]);
+  const [allLikedData, setAllLikedData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const fetchData = async () => {
+    setSelectedCategory("숙박");
+    const postsQ = query(collection(db, "posts"));
+    const postsQuerySnapshot = await getDocs(postsQ);
+    const postsData = postsQuerySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    //모든 포스트 데이터 저장
+    setPosts(postsData);
+
+    const likedQ = query(collection(db, "likes"));
+    const likedQuerySnapshot = await getDocs(likedQ);
+    const likedData = likedQuerySnapshot.docs.map((doc) => doc.data());
+    // 모든 좋아요 데이터 저장
+    setAllLikedData(likedData);
+  };
+  useEffect(() => fetchData, []);
+
+  // 리액트 쿼리로 로딩/에러 처리
+
+  const { isLoading, iserror, error } = useQuery("userData", fetchData);
+
+  if (isLoading) {
+    return <div>로딩 중입니다...</div>;
+  }
+
+  if (iserror) {
+    return alert(`에러 발생! Error Code: ${error.message}`);
+  }
 
   return (
     <div className="Container">
       <MainCarousel />
       <Category />
       <MiddleBanner />
-      <Preview />
+      <Preview
+        posts={posts}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        allLikedData={allLikedData}
+      />
       <EventBanner />
       <BestPlace />
     </div>
--- src/pages/nationpage/components/Preview.jsx
+++ src/pages/nationpage/components/Preview.jsx
@@ -1,74 +1,102 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import { db } from "../../../firebaseConfig";
-import { collection, getDocs, query } from "firebase/firestore";
+import { Link, useParams } from "react-router-dom";
 
-const Preview = () => {
+const Preview = ({
+  posts,
+  selectedCategory,
+  setSelectedCategory,
+  allLikedData,
+}) => {
+  const { nation } = useParams();
   const [selectedPosts, setSelectedPosts] = useState([]);
 
+  // 처음 컴포넌트 렌더링 될 때 숙박으로 카테고리 맞추고 리스트 불러오는 과정
+  const fetchData = async () => {
+    const filteredPosts = await posts.filter(
+      (post) => post.nation === nation && post.category === "숙박"
+    );
+    setSelectedPosts(filteredPosts);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // 버튼을 누르면 해당 nation, category 로 필터하여 데이터 가져옴
   const selectListHandler = async (category) => {
-    const postsQ = query(collection(db, "posts"));
-    const postsQuerySnapshot = await getDocs(postsQ);
-    const postsData = postsQuerySnapshot.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
-    console.log(postsData);
+    const filteredPosts = await posts.filter(
+      (post) => post.nation === nation && post.category === category
+    );
+    setSelectedPosts(filteredPosts);
   };
   return (
     <PreviewContainer>
       <h2>구경해봐요 또갈집</h2>
       <CategoryButtonContainer>
-        <CategoryButton onClick={() => selectListHandler("hotel")}>
-          숙소
+        <CategoryButton
+          onClick={() => {
+            selectListHandler("숙박");
+            setSelectedCategory("숙박");
+          }}
+          selected={selectedCategory === "숙박"}
+        >
+          숙박
         </CategoryButton>
-        <CategoryButton onClick={() => selectListHandler("food")}>
+        <CategoryButton
+          onClick={() => {
+            selectListHandler("맛집");
+            setSelectedCategory("맛집");
+          }}
+          selected={selectedCategory === "맛집"}
+        >
           맛집
         </CategoryButton>
-        <CategoryButton onClick={() => selectListHandler("tour")}>
+        <CategoryButton
+          onClick={() => {
+            selectListHandler("관광명소");
+            setSelectedCategory("관광명소");
+          }}
+          selected={selectedCategory === "관광명소"}
+        >
           관광명소
         </CategoryButton>
       </CategoryButtonContainer>
       <PreviewListContainer>
-        <PreviewListBox>
-          <ImageBox>이미지</ImageBox>
-          <LikesContainer>
-            <div className="likesBox">
-              <img src="icon/like_icon.svg" alt="likesIcon"></img>
-              <span>likes</span>
-            </div>
-            <div className="dislikesBox">
-              <img src="icon/dislike_icon.svg" alt="dislikesIcon"></img>
-              <span>dislikes</span>
-            </div>
-          </LikesContainer>
-        </PreviewListBox>
-        <PreviewListBox>
-          <ImageBox>이미지</ImageBox>
-          <LikesContainer>
-            <div className="likesBox">
-              <img src="icon/like_icon.svg" alt="likesIcon"></img>
-              <span>likes</span>
-            </div>
-            <div className="dislikesBox">
-              <img src="icon/dislike_icon.svg" alt="dislikesIcon"></img>
-              <span>dislikes</span>
-            </div>
-          </LikesContainer>
-        </PreviewListBox>
-        <PreviewListBox>
-          <ImageBox>이미지</ImageBox>
-          <LikesContainer>
-            <div className="likesBox">
-              <img src="icon/like_icon.svg" alt="likesIcon"></img>
-              <span>likes</span>
-            </div>
-            <div className="dislikesBox">
-              <img src="icon/dislike_icon.svg" alt="dislikesIcon"></img>
-              <span>dislikes</span>
-            </div>
-          </LikesContainer>
-        </PreviewListBox>
+        {/* 걸러진 데이터 중 3번째까지만 보여줌 */}
+        {selectedPosts.map((post, index) => {
+          if (index <= 2) {
+            // 이미지 중 1번째만 잡아서 백그라운드로 넣어줌
+            const imageUrl = post.postImgs[0];
+            const imageStyle = {
+              backgroundImage: `url(${imageUrl})`,
+            };
+            const likedCount = allLikedData?.filter(
+              (data) => data.postId === post.id && data.state === "like"
+            );
+            const dislikedCount = allLikedData?.filter(
+              (data) => data.postId === post.id && data.state === "dislike"
+            );
+            return (
+              <PreviewListBox to={`/detail/${post.id}`} key={post.id}>
+                <ImageBox style={imageStyle}></ImageBox>
+                <h3>{post.placeName}</h3>
+                {/* 추천/비추천 개수 보여줌 */}
+                <LikesContainer>
+                  <div className="likesBox">
+                    <img src="icon/like_icon.svg" alt="likesIcon"></img>
+                    <span>{likedCount.length}</span>
+                  </div>
+                  <div className="dislikesBox">
+                    <img src="icon/dislike_icon.svg" alt="dislikesIcon"></img>
+                    <span>{dislikedCount.length}</span>
+                  </div>
+                </LikesContainer>
+              </PreviewListBox>
+            );
+          }
+          return null;
+        })}
       </PreviewListContainer>
     </PreviewContainer>
   );
@@ -91,13 +119,16 @@ const CategoryButton = styled.div`
   width: 130px;
   height: 54px;
   margin: 60px 0 80px;
-  background-color: #e4e8e9;
-  color: #878d94;
+
+  /* selected가 현재 선택한 카테고리를 뜻함. 이게 true이면 파랗게 만듦 */
+  background-color: ${(props) => (props.selected ? "#0A58BE" : "#e4e8e9")};
+  color: ${(props) => (props.selected ? "#fff" : "#878d94")};
   font-size: 24px;
   font-weight: 400;
   line-height: 54px;
   text-align: center;
   transition: 0.3s;
+  cursor: pointer;
 
   &:first-child {
     border-top-left-radius: 60px;
@@ -111,8 +142,9 @@ const CategoryButton = styled.div`
     border-left: 1px solid #d7d7d7;
   }
 
+  /* 현재 선택된 버튼은 hover 되지 않도록 함 */
   &:hover {
-    background-color: #d5dadc;
+    background-color: ${(props) => (props.selected ? "#0A58BE" : "#d5dadc")};
   }
 `;
 
@@ -123,7 +155,7 @@ const PreviewListContainer = styled.div`
   gap: 70px;
 `;
 
-const PreviewListBox = styled.div`
+const PreviewListBox = styled(Link)`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -133,14 +165,17 @@ const PreviewListBox = styled.div`
 const ImageBox = styled.div`
   width: 380px;
   height: 380px;
-  margin-bottom: 20px;
   border-radius: 30px;
-  background-color: #999;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 const LikesContainer = styled.div`
   display: flex;
   gap: 8px;
+
+  margin-top: 20px;
   & > div {
     display: flex;
     align-items: center;
