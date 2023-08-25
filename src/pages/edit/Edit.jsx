c5a84bf136af338cb9e30d2fb142cc512780f7e2
unknown <hyunhe611@gmail.com>
2023-08-25 20:25:19 +0900

    feat/edit기능,댓글수정 #33

--- src/pages/edit/Edit.jsx
+++ src/pages/edit/Edit.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { db, storage } from '../../firebaseConfig';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import {
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  deleteObject,
+} from 'firebase/storage';
 
-function Edit() {
-  return <div>Edit</div>;
-}
+const Edit = () => {
+  const { id } = useParams();
+  const [post, setpost] = useState(null);
+  const [editDetail, setEditDetail] = useState('');
+  const [editImage, setEditImage] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const editPostData = doc(db, 'posts', id);
+      const docSnapshot = await getDoc(editPostData);
+      //detail페이지에서 전달 받은 텍스트
+      if (docSnapshot.exists()) {
+        setpost({ id: docSnapshot.id, ...docSnapshot.data() });
+        setEditDetail(docSnapshot.data().postContent);
+        console.log(docSnapshot.data().postContent);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  const handlePostChange = (e) => {
+    setEditDetail(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImages = Array.from(e.target.files);
+    setEditImage(selectedImages);
+  };
+
+  const handleImageDelete = async (imageUrl) => {
+    console.log('Deleting image:', imageUrl);
+    try {
+      // 이미지 Storage에서 삭제
+      const imageDelete = ref(storage, imageUrl);
+      await deleteObject(imageDelete);
+
+      // 이미지 firebase postImgs 배열에서 제거
+      const updatedImageUrls = post.postImgs.filter((url) => url !== imageUrl);
+
+      // Firestore 데이터베이스 업데이트
+      const editData = doc(db, 'posts', id);
+      await updateDoc(editData, { postImgs: updatedImageUrls });
+
+      // 컴포넌트의 상태 업데이트
+      setpost((prevPost) => ({ ...prevPost, postImgs: updatedImageUrls }));
+    } catch (error) {
+      console.error('이미지 삭제 오류:', error);
+    }
+  };
+
+  const handlePostSave = async () => {
+    try {
+      const editData = doc(db, 'posts', id);
+      // 업데이트할 필드와 값을 담을 빈 객체 생성
+      const updateData = {};
+
+      // 수정된 경우에만 업데이트
+      if (editDetail !== undefined) {
+        updateData.postContent = editDetail;
+      }
+
+      if (editImage && editImage.length > 0) {
+        const newImageUrls = [];
+
+        for (const image of editImage) {
+          const editImageData = ref(storage, `postImages/${id}/${image.name}`);
+          await uploadBytes(editImageData, image);
+          const editImageUrl = await getDownloadURL(editImageData);
+          newImageUrls.push(editImageUrl);
+        }
+
+        updateData.postImgs = [...post.postImgs, ...newImageUrls];
+      }
+
+      await updateDoc(editData, updateData);
+
+      navigate(`/detail/${id}`);
+    } catch (error) {
+      alert('게시물 수정 오류');
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      {post ? (
+        <div>
+          <div>Edit Post</div>
+          <div>
+            {post.postImgs.map((imageUrl) => (
+              <div key={imageUrl}>
+                <img
+                  src={imageUrl}
+                  alt="디테일 이미지"
+                  style={{ width: '200px', height: '200px' }}
+                />
+                <button onClick={() => handleImageDelete(imageUrl)}>x</button>
+              </div>
+            ))}
+          </div>
+          <textarea value={editDetail} onChange={handlePostChange} />
+          <br />
+          <input type="file" onChange={handleImageChange} multiple />
+          <br />
+          <button onClick={handlePostSave}>저장</button>
+        </div>
+      ) : (
+        <div>데이터 가져오는 중...</div>
+      )}
+    </div>
+  );
+};
 
 export default Edit;
