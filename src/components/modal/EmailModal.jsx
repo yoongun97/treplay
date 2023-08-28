ed88dc217feaa8c067135df82c3b1c3399935621
Cho Yoongeon <tesung5470@naver.com>
2023-08-28 17:31:12 +0900

    Feat / 이메일 중복, 이메일 오류 메시지 분리

--- src/components/modal/EmailModal.jsx
+++ src/components/modal/EmailModal.jsx
@@ -10,7 +10,7 @@ function EmailModal({
   // 밖에서체크한함수,
 }) {
   const [checkEmail, setCheckEmail] = useState(email);
-  const [isUsedEmail, setIsUsedEmail] = useState(false);
+  const [isUsedEmail, setIsUsedEmail] = useState("");
   const [showMsg, setShowMsg] = useState(true);
 
   useEffect(() => {
@@ -33,11 +33,14 @@ function EmailModal({
     try {
       const usedEmail = await fetchSignInMethodsForEmail(auth, checkEmail);
       console.log({ usedEmail });
-      setIsUsedEmail(usedEmail.length > 0);
+      if (usedEmail.length > 0) {
+        setIsUsedEmail("duplicate");
+      } else if (usedEmail.length === 0) {
+        setIsUsedEmail("notduplicate");
+      }
     } catch (error) {
-      // 중복된 이메일이 있는 경우
-      setIsUsedEmail(true);
       console.log(error);
+      setIsUsedEmail("error");
     }
   };
 
@@ -75,39 +78,51 @@ function EmailModal({
         </m.ModalCheckBtn>
       </m.ModalInputCheck>
 
-      {showMsg ? (
-        isUsedEmail === true ? (
-          <m.ModalErrorBox>
-            <m.ModalErrorMark
-              src="https://cdn-icons-png.flaticon.com/128/9503/9503179.png"
-              alt="경고이미지"
-            />
-            <m.ModalErrorMsg style={{ color: "#e02918" }}>
-              이미 사용중인 이메일 입니다.
-            </m.ModalErrorMsg>
-          </m.ModalErrorBox>
-        ) : (
-          <>
+      {showMsg && (
+        <div>
+          {isUsedEmail === "duplicate" ? (
             <m.ModalErrorBox>
               <m.ModalErrorMark
-                src="https://cdn-icons-png.flaticon.com/128/992/992481.png"
-                alt="가능이미지"
+                src="https://cdn-icons-png.flaticon.com/128/9503/9503179.png"
+                alt="경고이미지"
               />
-              <m.ModalErrorMsg>사용 가능한 이메일 입니다.</m.ModalErrorMsg>
+              <m.ModalErrorMsg style={{ color: "#e02918" }}>
+                이미 사용중인 이메일 입니다.
+              </m.ModalErrorMsg>
             </m.ModalErrorBox>
-            <m.SuccessBtn
-              onClick={() => {
-                setEmail(checkEmail);
-                setIsModalOpen(false);
-              }}
-            >
-              사용하기
-            </m.SuccessBtn>
-          </>
-        )
-      ) : (
-        <div style={{ height: "70px" }}></div>
+          ) : isUsedEmail === "notduplicate" ? (
+            <>
+              <m.ModalErrorBox>
+                <m.ModalErrorMark
+                  src="https://cdn-icons-png.flaticon.com/128/992/992481.png"
+                  alt="가능이미지"
+                />
+                <m.ModalErrorMsg>사용 가능한 이메일 입니다.</m.ModalErrorMsg>
+              </m.ModalErrorBox>
+              <m.SuccessBtn
+                onClick={() => {
+                  setEmail(checkEmail);
+                  setIsModalOpen(false);
+                }}
+              >
+                사용하기
+              </m.SuccessBtn>
+            </>
+          ) : (
+            <m.ModalErrorBox>
+              <m.ModalErrorMark
+                src="https://cdn-icons-png.flaticon.com/128/9503/9503179.png"
+                alt="경고이미지"
+              />
+              <m.ModalErrorMsg style={{ color: "#e02918" }}>
+                유효하지 않은 이메일입니다.
+              </m.ModalErrorMsg>
+            </m.ModalErrorBox>
+          )}
+        </div>
       )}
+
+      {!showMsg && <div style={{ height: "70px" }}></div>}
     </m.CheckModal>
   );
 }
