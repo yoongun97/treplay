92863a2e289cfde0505f97f8774b285bf8945389
yui62yui <you@example.com>
2023-09-01 12:12:25 +0900

    Style / 지도 검색결과 부분 스타일 적용 #68

--- src/components/place/PlaceSearch.jsx
+++ src/components/place/PlaceSearch.jsx
@@ -82,35 +82,42 @@ function PlaceSearch() {
 
   return isLoaded ? (
     <>
-      <SearchBox>
-        <input
-          type="text"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          placeholder="Enter an address to search"
-          onKeyDown={handleEnterKey}
-        />
-        <button onClick={handleSearch}>
-          <span></span>
-        </button>
-      </SearchBox>
-      <div>
-        {searchResults.map((result, index) => (
-          <div
-            key={index}
-            onClick={() => handleResultClick(result)}
-            style={{ cursor: "pointer", marginBottom: "5px" }}
-          >
-            {result.name} - {result.formatted_address}
-          </div>
-        ))}
-      </div>
-      {selectedResult && (
-        <div>
-          <p>선택된 장소명: {selectedResult.name}</p>
-          <p>선택된 주소: {selectedResult.formatted_address}</p>
-        </div>
-      )}
+      <SearchContainer>
+        <SearchBox>
+          <input
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="Enter an address to search"
+            onKeyDown={handleEnterKey}
+          />
+          <button onClick={handleSearch}>
+            <span></span>
+          </button>
+        </SearchBox>
+
+        <ResultBoxContainer>
+          {searchResults.map((result, index) => (
+            <ResultBox key={index} onClick={() => handleResultClick(result)}>
+              <span></span>
+              <div>
+                <p>{result.name}</p>
+                <p>{result.formatted_address}</p>
+              </div>
+            </ResultBox>
+          ))}
+        </ResultBoxContainer>
+        {selectedResult && (
+          <SelectedResultBox>
+            <span></span>
+            <div>
+              <p>{selectedResult.name}</p>
+              <p>{selectedResult.formatted_address}</p>
+            </div>
+          </SelectedResultBox>
+        )}
+      </SearchContainer>
+
       <div style={{ display: "none" }}>
         <GoogleMap
           mapContainerStyle={containerStyle}
@@ -128,15 +135,19 @@ function PlaceSearch() {
 
 export default PlaceSearch;
 
+const SearchContainer = styled.div`
+  width: 580px;
+  margin: 60px auto;
+`;
+
 const SearchBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 580px;
   height: 60px;
-  margin: 60px auto;
   padding-left: 20px;
   border: 1px solid #0a58be;
+  background-color: #fff;
   border-radius: 30px;
 
   & > input {
@@ -166,7 +177,86 @@ const SearchBox = styled.div`
     background-image: url(${process.env.PUBLIC_URL}/icon/search_icon.svg);
   }
 `;
-
+const ResultBoxContainer = styled.div`
+  border-radius: 30px;
+  background-color: #fff;
+  cursor: pointer;
+  overflow: hidden;
+`;
 const ResultBox = styled.div`
-  display: block;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  text-align: left;
+  background-color: #fff;
+
+  /* &:nth-child(2n) {
+    background-color: #f2f8ff;
+  } */
+
+  & > span {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    margin-right: 20px;
+    background: url(${process.env.PUBLIC_URL}/icon/map_icon_gray.svg) no-repeat
+      center / 100%;
+  }
+  & > div > p:first-child {
+    color: #222;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+  }
+  & > div > p:last-child {
+    color: #777;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 24px;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #e5e5e5;
+  }
+
+  &:first-child::after {
+    display: none;
+  }
+`;
+const SelectedResultBox = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  width: 580px;
+  margin-top: 10px;
+  text-align: left;
+
+  & > span {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    margin-right: 20px;
+    background: url(${process.env.PUBLIC_URL}/icon/map_icon_blue.svg) no-repeat
+      center / 100%;
+  }
+  & > div > p:first-child {
+    color: #222;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+  }
+  & > div > p:last-child {
+    color: #777;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 24px;
+  }
 `;
