3c60744984e79d3a0c66a08665f41d58ea7b198e
yui62yui <you@example.com>
2023-09-07 19:05:13 +0900

    Style / 메인캐러셀 css 적용 #68

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -18,24 +18,7 @@ function MainPage() {
         <MainHeader />
         <s.MainInner>
           <h2>어떤 여행지를 가고 싶으신가요?</h2>
-          {/* <s.ButtonContainer> */}
-          {/* <s.PrevButton />
-            <s.NationContainer>
-              <s.NationInner>
-                <s.StyledButton to={"/한국"}>
-                  <span>한국</span>
-                </s.StyledButton>
-                <s.StyledButton to={"/일본"}>
-                  <span>일본</span>
-                </s.StyledButton>
-                <s.StyledButton to={"/미국"}>
-                  <span>미국</span>
-                </s.StyledButton>
-              </s.NationInner>
-            </s.NationContainer>
-            <s.NextButton /> */}
           <MainCarousel />
-          {/* </s.ButtonContainer> */}
         </s.MainInner>
       </s.MainContainer>
       <Footer />
